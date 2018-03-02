c7b52744993eb9c62f951c1895de0a8160dcf26d
Ben Timby <btimby@smartfile.com>
2018-03-02 15:46:40 -0500

    More test coverage, functions working.

--- tests/test_rest.js
+++ tests/test_rest.js
@@ -2,17 +2,21 @@ const rest = require('../lib/rest');
 const nock = require('nock');
 const assert = require('assert');
 const morph = require('mock-env').morph;
+const streams = require('memory-streams');
+const fs = require('fs');
 
 
 API_URL = 'http://fakeapi.foo/'
 
 
 function assertHttpOK(e) {
+  // Assertion to ensure that error is omitted inside a callback.
   if (e) {
     console.log(e);
   }
-  assert(e === undefined);
+  assert(e === null);
 }
+
 
 describe('REST API client', () => {
   let client, server;
@@ -30,7 +34,15 @@ describe('REST API client', () => {
   });
   */
 
-  it('reads config from env', (done) => {
+  it('can read config from env', (done) => {
+    /*
+    This test instantiates a client without any options.
+
+    It then ensures the client has picked up configuration options from the
+    environment. However, we use mock-env.morph to only temporarily set
+    "environment" variables for this test.
+    */
+
     let alternate;
 
     morph(() => {
@@ -49,6 +61,13 @@ describe('REST API client', () => {
   });
 
   it('can ping api', (done) => {
+    /*
+    This test calls the ping API endpoint.
+
+    The JSON returned by the API is parsed and returned to callback as the
+    second parameter.
+    */
+
     server
       .get('/api/2/ping/')
       .reply(200, '{ "pong": "pong" }');
@@ -61,4 +80,63 @@ describe('REST API client', () => {
     });
   });
 
+  it('can pipe download to stream', (done) => {
+    /*
+    This test passes a stream to download().
+
+    When download() receives a stream, it pipes the downloaded file to that
+    stream. It calls callback when piping is complete, or if an error occurs.
+    */
+    let ws = new streams.WritableStream();
+
+    server
+      .get('/api/2/data/foobar')
+      .reply(200, 'BODY');
+
+    client.download('/foobar', (e) => {
+      assertHttpOK(e);
+      assert(ws.toString() === 'BODY');
+      done();
+    }, ws);
+  });
+
+  it('can pass download response to callback', (done) => {
+    /*
+    This test calls download with a callback only.
+
+    Without a stream, download() will invoke the callback for an error, or will
+    pass a second parameter (response). That response can be used to read the
+    downloaded file data.
+    */
+
+    server
+      .get('/api/2/data/foobar')
+      .reply(200, 'BODY');
+
+    client.download('/foobar', (e, r) => {
+      assertHttpOK(e);
+
+      r.on('data', (data) => {
+        assert(data.toString() === 'BODY');
+        done();
+      });
+    });
+  });
+
+  it('can upload a stream', (done) => {
+    // TODO: figure out why this is busted, works with a file!
+    //let rs = new streams.ReadableStream('BODY');
+    let rs = fs.createReadStream('/tmp/foo.txt');
+
+    server
+      .post('/api/2/data/foobar')
+      .reply(200, '{"size": 4, "name": "foobar", "path": "/foobar"}');
+
+    client.upload('/foobar', (e, json) => {
+      assertHttpOK(e);
+      assert(json.name === 'foobar');
+      done();
+    }, rs);
+
+  });
 });
