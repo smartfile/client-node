6371f87b4c62dbfa54353c5536929abe69c2f5bc
Ben Timby <btimby@smartfile.com>
2018-03-04 11:12:01 -0500

    FS tests. Add operations.

---
+++ tests/test_fs.js
@@ -0,0 +1,15 @@
+const nock = require('nock');
+const assert = require('assert');
+const morph = require('mock-env').morph;
+const streams = require('memory-streams');
+
+const fs = require('../lib/fs');
+
+
+describe('File System Abstraction', () => {
+
+  it('works', (done) => {
+    done();
+  });
+
+});
